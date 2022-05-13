f89741eaeb7e1434710da2e89c497f7eebdd45b6
upendar <upendar@LAPTOP-SSD99BUE>
2022-05-13 09:51:43 +0530

    commit

---
+++ Digits/src/FindDigit.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class  FindDigit
+	{
+			static int dc(int n)
+			{
+				if(n<=9)
+				{
+					return 1;
+				}
+				else
+				{
+					return 1+dc(n/10);
+				}
+			}
+	
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number");
+		 int n=sc.nextInt();
+        System.out.println(dc(n));
+	   
+	}
+	}
